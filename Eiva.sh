6fc0633455f775dc2bc250ba8c9443dc2c77a86e
Phoenix Error <88020281+PhoenixError@users.noreply.github.com>
2022-01-23 13:17:58 +0530

    Update Eiva.sh

--- Eiva.sh
+++ Eiva.sh
@@ -1,4 +1,4 @@
-git clone https://github.com/TeamEiva/Eiva-Userbot
-cd Eiva-Userbot
-python3 -m Eivabot
+git clone https://github.com/TeamAlphonse/Alphonse
+cd Alphonse
+python3 -m userbot
 # Team Eiva
